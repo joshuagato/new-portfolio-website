fa58b2a0da03b29568d41077512d01aab2782993
Joshua Gato <joshuagato37@gmail.com>
2026-02-01 20:18:36 +0000

    Added a reusables file

---
+++ src/utilities/reusables.js
@@ -0,0 +1,19 @@
+import { Github, Linkedin, Twitter, PhoneCallIcon } from "lucide-react";
+
+export const socialLinks = [
+    {icon: PhoneCallIcon, href: "tel: +233278223838", label: "Call"},
+    {icon: PhoneCallIcon, href: "https://api.whatsapp.com/send/?phone=233278223838&text&type=phone_number&app_absent=0", label: "WhatsApp"},
+    {icon: Github, href: "https://github.com/joshuagato", label: "GitHub"},
+    {icon: Linkedin, href: "https://www.linkedin.com/in/joshuagato", label: "LinkedIn"},
+    {icon: Twitter, href: "https://x.com/JoshuaGato5", label: "Twitter"},
+];
+
+export const footerLinks = [
+    {href: "#about", label: "About"},
+    {href: "#projects", label: "Projects"},
+    {href: "#experience", label: "Experience"},
+    {href: "#testimonials", label: "testimonials"},
+    {href: "#contact", label: "Contact"},
+];
+
+export const myFullName = "Joshua Gato";
