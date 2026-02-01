b26693a8101d4fa15f8f2d9b0215b72b34f2d914
Joshua Gato <joshuagato37@gmail.com>
2026-02-01 20:32:18 +0000

    Added new icons

--- src/utilities/reusables.js
+++ src/utilities/reusables.js
@@ -1,6 +1,7 @@
-import { Github, Linkedin, Twitter, PhoneCallIcon } from "lucide-react";
+import { Github, Linkedin, Twitter, PhoneCallIcon, MailsIcon } from "lucide-react";
 
 export const socialLinks = [
+    {icon: MailsIcon, href: "mailTo:joshuagato37@gmail.com", label: "Email"},
     {icon: PhoneCallIcon, href: "tel: +233278223838", label: "Call"},
     {icon: PhoneCallIcon, href: "https://api.whatsapp.com/send/?phone=233278223838&text&type=phone_number&app_absent=0", label: "WhatsApp"},
     {icon: Github, href: "https://github.com/joshuagato", label: "GitHub"},
@@ -8,7 +9,7 @@ export const socialLinks = [
     {icon: Twitter, href: "https://x.com/JoshuaGato5", label: "Twitter"},
 ];
 
-export const footerLinks = [
+export const headerAndFooterLinks = [
     {href: "#about", label: "About"},
     {href: "#projects", label: "Projects"},
     {href: "#experience", label: "Experience"},
