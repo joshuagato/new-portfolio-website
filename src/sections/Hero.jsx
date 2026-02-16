d1f152b5b7da9bfb0f868d8136237cf67bd39aa1
Joshua Gato <joshuagato37@gmail.com>
2026-02-16 00:17:36 +0000

    Changed the picture in the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -106,7 +106,7 @@ export const Hero = () => {
                         />
                         <div className="relative glass rounded-3xl p-2 glow-border">
                             <img
-                                src="/profile-photo.png"
+                                src="/profile-photo.jpeg"
                                 alt="Joshua Gato"
                                 className="w-full aspect-[4/5] object-contain rounded-2xl"
                             />
